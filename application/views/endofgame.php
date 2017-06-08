13c7b1614f5bcb6d044a3202fb671026a04deb32
Jilvin Jacob <jilvinjacob@gmail.com>
2017-06-09 01:17:31 +0530

    changes in endofgame.php file

--- application/views/endofgame.php
+++ application/views/endofgame.php
@@ -2,7 +2,7 @@
 <div class="container">
 <div class="row">
 <div class="col-md-12 wrapper">
-The game has officially reached an end at 19 March 2017 00:00 IST. Thanks for participating. Winner will be announced soon.
+The game has officially reached an end at 19 March 2017 00:00 IST. Thanks for participating. Check the leaderboard to know the winners. See you next time.
 </div>
 </div>
 </div>
